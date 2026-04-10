eaaabdfe4d47221e7b294a79be5723bc00af598d
kweeck <kweeck2@yandex.ru>
2026-04-10 15:30:29 +0300

    final version, need to customize path

--- sql/01_raw.sql
+++ sql/01_raw.sql
@@ -3,4 +3,4 @@ CREATE SCHEMA IF NOT EXISTS staging;
 CREATE SCHEMA IF NOT EXISTS mart;
 
 CREATE TABLE IF NOT EXISTS raw.loans AS
-SELECT * FROM read_csv_auto("C:/Users/kweec/python files/risk-project/data/accepted_2007_to_2018Q4.csv", ignore_errors=true);
+SELECT * FROM read_csv_auto("C:/Users/kweec/python_files/risk-project/data/accepted_2007_to_2018Q4.csv", ignore_errors=true);
